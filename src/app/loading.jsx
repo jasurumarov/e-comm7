52adafccaa861129efb502dbe728234212745a8d
jasurumarov <jasurumarov199@gmail.com>
2024-06-01 02:42:15 +0500

    all thing up to date

---
+++ src/app/loading.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+const loading = () => {
+    return (
+        <section className='loading-section'>
+            <div className="container">
+                <div className="products-section__content">
+                    <div className="products__cards">
+                        <div className="products__card">
+                            <div className="products__card-img"></div>
+                            <h3></h3>
+                            <div className='products__card-stars'></div>
+                            <div className="products__card-prices"></div>
+                        </div>     <div className="products__card">
+                            <div className="products__card-img"></div>
+                            <h3></h3>
+                            <div className='products__card-stars'></div>
+                            <div className="products__card-prices"></div>
+                        </div>     <div className="products__card">
+                            <div className="products__card-img"></div>
+                            <h3></h3>
+                            <div className='products__card-stars'></div>
+                            <div className="products__card-prices"></div>
+                        </div>     <div className="products__card">
+                            <div className="products__card-img"></div>
+                            <h3></h3>
+                            <div className='products__card-stars'></div>
+                            <div className="products__card-prices"></div>
+                        </div>     <div className="products__card">
+                            <div className="products__card-img"></div>
+                            <h3></h3>
+                            <div className='products__card-stars'></div>
+                            <div className="products__card-prices"></div>
+                        </div>     <div className="products__card">
+                            <div className="products__card-img"></div>
+                            <h3></h3>
+                            <div className='products__card-stars'></div>
+                            <div className="products__card-prices"></div>
+                        </div>     <div className="products__card">
+                            <div className="products__card-img"></div>
+                            <h3></h3>
+                            <div className='products__card-stars'></div>
+                            <div className="products__card-prices"></div>
+                        </div>     <div className="products__card">
+                            <div className="products__card-img"></div>
+                            <h3></h3>
+                            <div className='products__card-stars'></div>
+                            <div className="products__card-prices"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default loading
