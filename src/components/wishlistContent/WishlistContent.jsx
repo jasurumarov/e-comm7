2f9e2295df67ad5fcf048af85ece06fc155887d2
jasurumarov <jasurumarov199@gmail.com>
2024-05-31 21:06:39 +0500

    all thing up to date

--- src/components/wishlistContent/WishlistContent.jsx
+++ src/components/wishlistContent/WishlistContent.jsx
@@ -4,14 +4,16 @@ import { useSelector } from 'react-redux'
 import Products from '@/components/products/Products'
 import Empty from '@/components/empty/Empty'
 
-const WishlistContent = () => {
-    const data = useSelector(state => state.wishlist.value)
 
+const WishlistContent = () => {
+    let data = useSelector(state => state.wishlist.value)
     return (
         <div>
-            {data.length 
-                ? <Products data={data} category={[]} />
-                : <Empty data={'wishlist'} />
+            {data.length
+                ?
+                <Products data={data} category={[]} />
+                :
+                <Empty data={'wishlist'}/>
             }
         </div>
     )
